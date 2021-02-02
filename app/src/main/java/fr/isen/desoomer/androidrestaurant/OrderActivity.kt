28ade8fb83f4c2b0970982663828211d36655c38
DESOOMER Pierre <p.desoomer@gmail.com>
2021-02-02 14:00:58 +0100

    Added order status

--- app/src/main/java/fr/isen/desoomer/androidrestaurant/OrderActivity.kt
+++ app/src/main/java/fr/isen/desoomer/androidrestaurant/OrderActivity.kt
@@ -5,21 +5,30 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import com.airbnb.lottie.LottieAnimationView
+import com.airbnb.lottie.LottieDrawable
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.google.gson.Gson
+import fr.isen.desoomer.androidrestaurant.databinding.ActivityOrderBinding
 import fr.isen.desoomer.androidrestaurant.domain.UserData
 import org.json.JSONException
 import org.json.JSONObject
 
+private lateinit var binding: ActivityOrderBinding;
 class OrderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_order)
+        binding = ActivityOrderBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         sendOrderToAPI()
-        var cart_icon = findViewById<LottieAnimationView>(R.id.waiting_for_delivery)
-        cart_icon.playAnimation()
+        var waiting_for_delivery = findViewById<LottieAnimationView>(R.id.waiting_for_delivery)
+        waiting_for_delivery.playAnimation()
+
+        var delivering = findViewById<LottieAnimationView>(R.id.delivering)
+        delivering.visibility = View.GONE
+
+
     }
 
     fun sendOrderToAPI() {
@@ -39,11 +48,19 @@ class OrderActivity : AppCompatActivity() {
             postData,
             { response ->
                 Log.i("Reponse : ", ""+response)
-                var cart_icon = findViewById<LottieAnimationView>(R.id.waiting_for_delivery)
+                var cart_icon = binding.waitingForDelivery
                 cart_icon.visibility = View.GONE
+
+                var delivering = binding.delivering
+                delivering.visibility = View.VISIBLE
+                delivering.playAnimation()
+                delivering.loop(true)
+
+                binding.orderStatus.setText("Order success !")
             },
             { error ->
                 error.printStackTrace()
+                binding.orderStatus.setText("Order failed, please try again !")
             })
         requestQueue.add(jsonObjectRequest)
     }
