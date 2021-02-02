c522c9523db266109921a987855141868f4827a9
DESOOMER Pierre <p.desoomer@gmail.com>
2021-02-02 12:01:03 +0100

    We are now sending data to the API with Cart, User_id and Shop_id

--- app/src/main/java/fr/isen/desoomer/androidrestaurant/OrderActivity.kt
+++ app/src/main/java/fr/isen/desoomer/androidrestaurant/OrderActivity.kt
@@ -3,10 +3,42 @@ package fr.isen.desoomer.androidrestaurant
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.google.gson.Gson
+import fr.isen.desoomer.androidrestaurant.domain.UserData
+import org.json.JSONException
+import org.json.JSONObject
 
 class OrderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order)
+        sendOrderToAPI()
+    }
+
+    fun sendOrderToAPI() {
+        val postUrl = "http://test.api.catering.bluecodegames.com/user/order"
+        val requestQueue = Volley.newRequestQueue(this)
+        val postData = JSONObject()
+        try {
+            postData.put("id_shop", "1")
+            postData.put("id_user", getSharedPreferences(DishDetailActivity.APP_PREFS, MODE_PRIVATE).getInt("user_id", 0))
+            postData.put("msg", intent.getSerializableExtra("Cart"))
+        } catch (e: JSONException) {
+            e.printStackTrace()
+        }
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.POST,
+            postUrl,
+            postData,
+            { response ->
+                Log.i("Reponse : ", ""+response)
+            },
+            { error ->
+                error.printStackTrace()
+            })
+        requestQueue.add(jsonObjectRequest)
     }
 }
